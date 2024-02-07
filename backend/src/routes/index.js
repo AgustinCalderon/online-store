121ac4b901d352a2e900454c150a6123ca0cf4ca
AgustinCalderon <agustincalderon058@gmail.com>
2024-02-07 14:25:23 -0300

    create route for products by id

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const products = require('../controller/index');
 
-router.get('/products', products.getAll);
+router.get('/products', products.getAll)
+router.get('/products/:id', products.getById)
 
 module.exports = router
