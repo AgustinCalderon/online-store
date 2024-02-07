a2009abb0113b55866c0524d7427333d954d82c2
AgustinCalderon <agustincalderon058@gmail.com>
2024-02-07 09:35:13 -0300

    create a funtion to view products from bd to page

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,7 +1,36 @@
 const $ = document;
 const containerProducts = $.querySelector('#containerProducts') 
 
-fetch('http://localhost:8000/products')
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error))
+const cartStructure = () => ({title, image, description, price}) => {
+    return `<div class="cart">
+    <h3 class="cart-title">${title}</h3>
+    <p class="cart-desc" >${description}</p>
+    <img class="cart-img" src="${image}">
+    <strong class="cart-price">${price}</strong>
+    <button class="cart-button">Add Cart</button>
+    </div>`
+}
+
+const renderCarts = (products) => {
+    containerProducts.innerHTML += ``    
+    products.map(item => {
+        containerProducts.innerHTML += cartStructure()(item)
+    })
+}
+
+const getAllProducts = async () => {
+
+    try {
+        const response = await fetch('http://localhost:8000/products')
+        if (!response.ok) {
+            throw new Error('Network response was not ok')
+        } 
+        const data = await response.json()
+        renderCarts(data)
+    }
+    catch (error) {
+        alert('There has been a problem with your fetch operation: ', error.message)
+    }
+}
+
+getAllProducts()
