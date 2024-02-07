da0a2156af666280dc3c7453468f49670fcd81f8
AgustinCalderon <agustincalderon058@gmail.com>
2024-02-07 14:23:16 -0300

    create query to get product by id

--- backend/src/model/index.js
+++ backend/src/model/index.js
@@ -1,6 +1,7 @@
 const {connectToMongoDB, disconnectToMongoDB} = require('../setting/index')
 
 class ProcductModel {
+    
     static async getAll() {
         try {
             const clientMongo = await connectToMongoDB()
@@ -14,6 +15,24 @@ class ProcductModel {
             console.log(dataArray)
             await disconnectToMongoDB()
             return {data: dataArray, error: false}
+        } catch (error) {
+            return error
+        }
+    }
+
+    static async getById(id) {
+        try {
+            const clientMongo = await connectToMongoDB()
+            if (!clientMongo) {
+                return Error('Error connecting to the database')
+            }
+            const data = await clientMongo.db('store').collection('products').findOne({id: id})
+            if (!data) {
+                return {data: null, error: true}
+            }
+            console.log(data)
+            await disconnectToMongoDB()
+            return {data: data, error: false}
         } catch (error) {
             return error
         }
