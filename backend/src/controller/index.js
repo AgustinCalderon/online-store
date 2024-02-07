01340c0a69ecbc8c48b76be924af68bfaacae194
AgustinCalderon <agustincalderon058@gmail.com>
2024-02-07 14:24:54 -0300

    Added method for get product by id

--- backend/src/controller/index.js
+++ backend/src/controller/index.js
@@ -1,11 +1,21 @@
 const ProcductModel = require('../model/index');
 
 class Products{
+    
     static async getAll(req, res){
         const {data, error} = await ProcductModel.getAll()        
         error ? res.status(400).json({error: 'products not found'})
               : res.status(200).json(data);
-    } 
+    }
+    
+    static async getById(req, res){
+        const {id} = req.params;
+        if (!id) {
+            return res.status(400).json({error: 'id is required'})
+        }
+        const {data, error} = await ProcductModel.getById(Number(id))
+        error ? res.status(400).json({error: 'product not found'})
+              : res.status(200).json(data);
+    }
 }
-
 module.exports = Products;
